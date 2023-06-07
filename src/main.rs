1a8e4e7bc7a545b8fc2cdc10fc5d28bd39abce34
saurabh <meetsaurabhdivine@gmail.com>
2023-06-08 00:41:22 +0530

    Add chat feature

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,70 @@
-#[macro_use]
-extern crate rocket;
+#[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "any change"
+#[cfg(test)] mod tests;
+
+use rocket::{State, Shutdown};
+use rocket::fs::{relative, FileServer};
+use rocket::form::Form;
+use rocket::response::stream::{EventStream, Event};
+use rocket::serde::{Serialize, Deserialize};
+use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
+use rocket::tokio::select;
+
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[cfg_attr(test, derive(PartialEq, UriDisplayQuery))]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    #[field(validate = len(..30))]
+    pub room: String,
+    #[field(validate = len(..20))]
+    pub username: String,
+    pub message: String,
 }
 
-#[shuttle_runtime::main]
-async fn rocket() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index]);
+/// Returns an infinite stream of server-sent events. Each event is a message
+/// pulled from a broadcast queue sent by the `post` handler.
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = rx.recv() => match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
 
-    Ok(rocket.into())
+            yield Event::json(&msg);
+        }
+    }
 }
+
+/// Receive a message from a form submission and broadcast it to any receivers.
+#[post("/message", data = "<form>")]
+fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+    // A send 'fails' if there are no active subscribers. That's okay.
+    let _res = queue.send(form.into_inner());
+}
+
+#[launch]
+async fn rocket() -> shuttle_rocket::ShuttleRocket{
+    rocket::build()
+        .manage(channel::<Message>(1024).0)
+        .mount("/", routes![post, events])
+        .mount("/", FileServer::from(relative!("static")));
+        let rocket = rocket::build().mount("/", routes![index]);
+}
+
+
+// #[shuttle_runtime::main]
+// async fn rocket() -> shuttle_rocket::ShuttleRocket {
+//     .manage(state: channel::<Messages>(capacity: 1024).0)
+//     .mount("/", routes![post, events])
+//     .mount("/", FileServer::from(relative!("static")))
+//     let rocket = rocket::build().mount("/", routes![index]);
+
+//     Ok(rocket.into())
+// }
